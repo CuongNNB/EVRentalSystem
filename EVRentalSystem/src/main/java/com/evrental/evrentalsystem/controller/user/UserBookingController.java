182a6520ec030bb91bd79413dbffb908be908323
tn-1412 <trungnguyen14122005@gmail.com>
2025-10-16 16:43:07 +0700

    - Sửa lại lỗi sai logic trạng thái BookingService

--- EVRentalSystem/src/main/java/com/evrental/evrentalsystem/controller/user/UserBookingController.java
+++ EVRentalSystem/src/main/java/com/evrental/evrentalsystem/controller/user/UserBookingController.java
@@ -1,22 +1,22 @@
-package com.evrental.evrentalsystem.controller.user;
+    package com.evrental.evrentalsystem.controller.user;
 
 
-import com.evrental.evrentalsystem.request.BookingRequest;
-import com.evrental.evrentalsystem.response.user.BookingResponseDTO;
+    import com.evrental.evrentalsystem.request.BookingRequest;
+    import com.evrental.evrentalsystem.response.user.BookingResponseDTO;
 
-import com.evrental.evrentalsystem.service.BookingService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+    import com.evrental.evrentalsystem.service.BookingService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/user")
-@RequiredArgsConstructor
-public class UserBookingController {
+    @RestController
+    @RequestMapping("/api/user")
+    @RequiredArgsConstructor
+    public class UserBookingController {
 
-    private final BookingService bookingService;
-    //API: http://localhost:8084/EVRentalSystem/api/user/booking
-    @PostMapping("/booking")
-    public BookingResponseDTO createBooking(@RequestBody BookingRequest request) {
-        return bookingService.createBooking(request);
+        private final BookingService bookingService;
+        //API: http://localhost:8084/EVRentalSystem/api/user/booking
+        @PostMapping("/booking")
+        public BookingResponseDTO createBooking(@RequestBody BookingRequest request) {
+            return bookingService.createBooking(request);
+        }
     }
-}
