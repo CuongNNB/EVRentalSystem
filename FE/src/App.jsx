0b4a6cd7013cb4d89cb98afb27b378fde434dd5e
Nguyen Ngoc Bao Cuong <baocuongg@gmail.com>
2025-10-20 08:27:53 +0700

    Xóa các trang thừa

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -10,25 +10,17 @@ import CarPages from "./pages/CarPages";
 import CarDetail from "./pages/CarDetail";
 import BookingPage from "./pages/BookingPage";
 import ContractPage from "./pages/ContractPage";
-import ContractSuccessPage from "./pages/ContractSuccessPage";
 import DepositPaymentPage from "./pages/DepositPaymentPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import PaymentSuccessPage from "./pages/PaymentSuccessPage";
-import ContractDemoPage from "./pages/ContractDemoPage";
-import DemoFlowPage from "./pages/DemoFlowPage";
 import DashboardUser from "./pages/DashboardUser";
 import CustomerInfoPage from "./pages/CustomerInfoPage";
 import StaffLayout from "./pages/staff/StaffLayout";
 import OrdersList from "./pages/staff/Orders/OrdersList";
 import OrderDetail from "./pages/staff/Orders/OrderDetail";
 import HandoverCar from "./pages/staff/Orders/HandoverCar";
-import ReturnCar from "./pages/staff/Orders/CheckCar";
 import ExtraFee from "./pages/staff/Orders/ExtraFee";
 import StationCarView from "./pages/StationCarView";
 import MapStationsDemo from "./pages/MapStations";
-import TestPage from "./pages/TestPage";
 import CheckCar from "./pages/staff/Orders/CheckCar";
-
 import UserContract from "./pages/renter/UserContract";
 import MyBookings from "./pages/MyBookings";
 import BookingDetailHistory from "./pages/BookingDetailHistory.jsx";
@@ -42,34 +34,40 @@ function App() {
             <CheckoutProvider>
                 <Router>
                     <Routes>
-                        <Route path="/user-contract" element={<UserContract/>} />
-                        <Route path="/" element={<Homepage />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/dashboard" element={<ProtectedRoute><DashboardUser /></ProtectedRoute>} />
-                        <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
-                        <Route path="/my-bookings/:id" element={<ProtectedRoute><BookingDetailHistory /></ProtectedRoute>} />
-                        <Route path="/account" element={<ProtectedRoute><UserProfilePage /></ProtectedRoute>} />
-                        <Route path="/cars" element={<CarPages />} />
-                        <Route path="/car/:id" element={<CarDetail />} />
-                        <Route path="/booking/:carId" element={<BookingPage />} />
-                        <Route path="/contract/:carId" element={<ContractPage />} />
-                        <Route path="/contract-success" element={<ProtectedRoute><ContractSuccessPage /></ProtectedRoute>} />
-                        <Route path="/deposit-payment" element={<ProtectedRoute><DepositPaymentPage /></ProtectedRoute>} />
-                        <Route path="/customer-info" element={<ProtectedRoute><CustomerInfoPage /></ProtectedRoute>} />
-                        <Route path="/checkout" element={<CheckoutPage />} />
-                        <Route path="/checkout/success" element={<PaymentSuccessPage />} />
-                        <Route path="/contract-demo" element={<ContractDemoPage />} />
-                        <Route path="/demo-flow" element={<DemoFlowPage />} />
+                        //Phần trang của Renter
+                        <Route path="/user-contract" element={<UserContract/>}/> //Trang Hợp đồng người thuê
+                        <Route path="/" element={<Homepage/>}/> //Trang Homepage
+                        <Route path="/login" element={<Login/>}/> //Trang Đăng nhập
+                        <Route path="/register" element={<Register/>}/> //Trang Đăng ký
+                        <Route path="/dashboard" element={<ProtectedRoute><DashboardUser/></ProtectedRoute>}/> //Trang
+                        Dashboard người dùng
+                        <Route path="/my-bookings" element={<ProtectedRoute><MyBookings/></ProtectedRoute>}/> //Trang
+                        Đơn đặt xe của tôi
+                        <Route path="/my-bookings/:id"
+                               element={<ProtectedRoute><BookingDetailHistory/></ProtectedRoute>}/> //Trang Chi tiết đơn
+                        đặt xe
+                        <Route path="/account" element={<ProtectedRoute><UserProfilePage/></ProtectedRoute>}/> //Trang
+                        Hồ sơ người dùng
+                        <Route path="/cars" element={<CarPages/>}/> //Trang danh sách xe có sẵn
+                        <Route path="/car/:id" element={<CarDetail/>}/> //Trang Chi tiết xe
+                        <Route path="/booking/:carId" element={<BookingPage/>}/> //Trang Đặt xe
+                        <Route path="/contract/:carId" element={<ContractPage/>}/> //Trang Hợp đồng
+                        <Route path="/deposit-payment"
+                               element={<ProtectedRoute><DepositPaymentPage/></ProtectedRoute>}/> //Trang Thanh toán đặt
+                        cọc
+                        <Route path="/customer-info"
+                               element={<ProtectedRoute><CustomerInfoPage/></ProtectedRoute>}/> //Trang Thông tin khách
+                        hàng
+                        <Route path="/station-cars" element={<StationCarView/>}/>
+                        <Route path="/map-stations" element={<MapStationsDemo/>}/>
+
+                        //Phần trang của Staff
                         <Route path="/staff" element={<ProtectedRoute><StaffLayout /></ProtectedRoute>} />
                         <Route path="/staff/orders" element={<ProtectedRoute><OrdersList /></ProtectedRoute>} />
                         <Route path="/staff/orders/:orderId" element={<ProtectedRoute><OrderDetail /></ProtectedRoute>} />
                         <Route path="/staff/orders/:orderId/handover" element={<ProtectedRoute><HandoverCar /></ProtectedRoute>} />
                         <Route path="/staff/orders/:orderId/handover/check" element={<ProtectedRoute><CheckCar /></ProtectedRoute>} />
                         <Route path="/staff/orders/:orderId/extra-fee" element={<ProtectedRoute><ExtraFee /></ProtectedRoute>} />
-                        <Route path="/station-cars" element={<StationCarView />} />
-                        <Route path="/map-stations" element={<MapStationsDemo />} />
-                        <Route path="/test" element={<TestPage />} />
                     </Routes>
                 </Router>
             </CheckoutProvider>
@@ -78,4 +76,3 @@ function App() {
 }
 
 export default App;
-// Compare this snippet from src/pages/ContractDemoPage.jsx:
