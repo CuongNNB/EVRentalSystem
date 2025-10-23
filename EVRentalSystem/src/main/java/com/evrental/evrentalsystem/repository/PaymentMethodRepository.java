e966105eaffb4504614e1291a92da4b93be0ba3f
Nguyen Ngoc Bao Cuong <baocuongg@gmail.com>
2025-10-23 07:32:33 +0700

    Bỏ bảng Payment Method

--- EVRentalSystem/src/main/java/com/evrental/evrentalsystem/repository/PaymentMethodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.evrental.evrentalsystem.repository;
-
-import com.evrental.evrentalsystem.entity.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Integer> {
-}
