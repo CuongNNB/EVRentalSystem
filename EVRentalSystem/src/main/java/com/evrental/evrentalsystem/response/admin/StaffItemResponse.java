fadf7afdbb8ebf38b8289087992727e1ba53798c
tn-1412 <trungnguyen14122005@gmail.com>
2025-10-30 16:15:46 +0700

    .

---
+++ EVRentalSystem/src/main/java/com/evrental/evrentalsystem/response/admin/StaffItemResponse.java
@@ -0,0 +1,48 @@
+package com.evrental.evrentalsystem.response.admin;
+
+import lombok.*;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StaffItemResponse {
+
+    private List<StaffItem> data;
+
+    private Kpis kpis;
+
+    private int page;
+    private int size;
+    private long total;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class StaffItem {
+        private Integer id;
+        private String name;
+        private String email;
+        private String position;
+        private String status;
+        private Integer stationId;
+        private String stationName;
+
+        private Integer handovers;
+        private Double avgRating;
+        private Integer onTimeRate;
+        private Integer customerSatisfaction;
+        private Integer shiftsThisMonth;
+        private Integer shiftsTotal;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Kpis {
+        private long totalStaff;
+        private long activeCount;
+        private double avgRating;
+        private long totalHandovers;
+    }
+}
